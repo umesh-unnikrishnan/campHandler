c2cdbe6a82f7f504c21002b4ab26fb1b542dd26c
Kevin <kevin@ad.infosys.com>
2018-08-19 15:21:17 +0530

    Camp Admin Module

--- js/app/campAdminController.js
+++ js/app/campAdminController.js
@@ -1,3 +1,20 @@
 application.controller('campAdminController',function($scope, $http, $timeout, $mdSidenav) {
-
+    $scope.loggedInUser = {
+        name : "Camp Admin",
+        campName : "Kuttur LP School"
+    }
+    $scope.campDetails = {
+        campName : "Kuttur LP School",
+        headCount : 1530,
+        male : 700,
+        female : 600,
+        kids : 230,
+        notCategorised : 230
+    }
+    $scope.units = ["Kgs" ,"Ltrs" , "Nos"];
+    $scope.severityList = ["High" ,"Medium" , "Low"];
+    $scope.tempRequirement = {
+        unit : $scope.units[0],
+        severity : $scope.severityList[0]
+    }
 });
